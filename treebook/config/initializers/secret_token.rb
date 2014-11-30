7ffc772bcd0c0f6b2fb5264d6f46eb38caba2721
Rajat Bhageria <rajat1996@gmail.com>
2014-11-30 01:27:59 -0500

    Created the statuses

---
+++ treebook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'ef70bde400ab9860a0b4125f52e4fc7fa724c001394f303251662f6ce6fdfe13a537cc18c1fa060b5ce25dbbd3a30ad18ce5a6a3f4af13b446bf960eec6a7951'
